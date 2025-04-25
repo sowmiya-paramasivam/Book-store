876cbb9b0efa9cea619beb4b0ceff2916d237128
Sowmiya Paramasivam <your-email@example.com>
2025-04-25 00:25:16 -0700

    changes

--- client/src/home/OtherBooks.jsx
+++ client/src/home/OtherBooks.jsx
@@ -5,7 +5,7 @@ const OtherBooks = () => {
     const [Books,setBooks] =useState([]);
 
     useEffect( ()=>{
-      fetch(`${import.meta.env.VITE_API_BASE_URL}/all-books`).then(res=>res.json()).then(data=>setBooks(data.slice(0,8)))
+      fetch(`${import.meta.env.VITE_API_BASE_URL}/all-books`).then(res=>res.json()).then(data=>setBooks(data.slice(5,8)))
     },[]);
   return (
     <div>
