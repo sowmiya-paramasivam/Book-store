0fd7b17f5e43857e5cd06122958f2445c49183e8
Sowmiya Paramasivam <your-email@example.com>
2025-04-26 00:43:54 -0700

    changes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(cors({
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log('❌ Blocked by CORS:', origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
